c2be648c4b6f33bed6622ba87d8ef9d2bc92dde2
Ibrahim Gabr <igabr@uchicago.edu>
2017-06-14 17:27:48 -0500

    Add files via upload

---
+++ linked_lst.py
@@ -0,0 +1,53 @@
+class Node(object):
+
+	def __init__(self, data=None, pointer=None):
+		self.data = data
+		self.pointer = pointer
+
+	def get_data(self):
+		return self.data
+
+	def get_pointer(self):
+		return self.pointer
+
+	def set_pointer(self, new_pointer):
+		self.pointer = new_pointer
+
+class LinkedList(object):
+
+	def __init__(self, root=None):
+		self.root = root
+
+	def insert(self, data):
+		new_node = Node(data)
+		new_node.set_pointer(self.root)
+		self.root = new_node
+
+	@property
+	def size(self):
+		current = self.root
+		count = 0
+
+		while current:
+			count += 1
+			current = current.get_pointer()
+		return count
+	
+	def search(self, data):
+		current = self.root
+
+		while current:
+			if current.get_data() == data:
+				return "item is in lst"
+			else:
+				current = current.get_pointer()
+		return "Item not in lst"
+
+	@property
+	def print_lst(self):
+		lst = []
+		current = self.root
+		while current:
+			lst.append(str(current.get_data()))
+			current = current.get_pointer()
+			print( '->'.join(lst))
