feb607191fb8bdfe3539aed561c2d3ff7f0df591
Ibrahim Gabr <igabr@uchicago.edu>
2017-06-24 06:09:31 -0500

    Add files via upload

---
+++ lottery.py
@@ -0,0 +1,52 @@
+#Ibrahim Gabr
+import random
+import os
+import sys
+
+"""
+Simple script that will generate random lottery numbers for Euromillions and National
+Lottery. Both games played in the UK. Think of this script as being the same as 'Lucky Dip'.
+
+Usage:
+	Go to terminal and type "python3 lottery.py"
+
+Enjoy
+"""
+
+def euromillions():
+
+	for i in range(5):
+		main= random.sample(range(1, 51), 5) #range is not inclusive
+		aux = random.sample(range(1,13), 2) #range is not inclusive
+		main.append(aux)
+		print(main)
+
+def lotto():
+
+	for i in range(5):
+		main = random.sample(range(1, 60), 6) #range is not inclusive
+		print(main)
+
+if __name__ == '__main__':
+	print()
+	print("Here are the euromillions numbers: ")
+	print()
+	euromillions()
+	print()
+	print("Here are the lotto numbers: ")
+	print()
+	lotto()
+	question = input("""
+	Would you like to generate numbers again?
+
+	1. Yes
+
+	2. No
+
+	Answer: """)
+	if question == "1":
+		os.system("clear")
+		os.system("python3 lottery.py")
+	else:
+		os.system("clear")
+		sys.exit()
