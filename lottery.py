a37f64c8bc4d0b12be9f0c471b2e4f7fbd34d9fb
Ibrahim Gabr <igabr@uchicago.edu>
2017-06-30 14:10:41 -0500

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -2,6 +2,7 @@
 import random
 import os
 import sys
+import string
 
 """
 Simple script that will generate random lottery numbers for Euromillions and National
@@ -13,40 +14,67 @@ Usage:
 Enjoy
 """
 
-print()
-euro_lines = input("How many lines for Euromillions would you like to generate? ")
-print()
-lotto_lines = input("How many lines for National Lottery would you like to generate? ")
+def get_euro():
+	euro_lines = input("How many lines for Euromillions would you like to generate? ")
 
-def euromillions():
+	try:
+		val = int(euro_lines)
+		return val
+	except ValueError:
+		print()
+		print("Not a valid number - please enter an integer!")
+		print()
+		return get_euro()
 
-	if int(euro_lines) <= 0:
+
+def get_lotto():
+
+	lotto_lines = input("How many lines for National Lottery would you like to generate? ")
+
+	try:
+		val = int(lotto_lines)
+		return val
+	except ValueError:
+		print()
+		print("Not a valid number - please enter an integer")
+		print()
+		return get_lotto()
+
+def euromillions(lines):
+
+	if lines <= 0:
 		print("No lines generated")
 	else:
-		for i in range(int(euro_lines)):
+		for i in range(lines):
 			main= random.sample(range(1, 51), 5) #range is not inclusive
 			aux = random.sample(range(1,13), 2) #range is not inclusive
 			main.append(aux)
 			print(main)
 
-def lotto():
+def lotto(lines):
 
-	if int(lotto_lines) <= 0:
+	if lines <= 0:
 		print("No lines generated")
 	else:
-		for i in range(int(lotto_lines)):
+		for i in range(lines):
 			main = random.sample(range(1, 60), 6) #range is not inclusive
 			print(main)
 
 if __name__ == '__main__':
 	print()
+	euro_range = get_euro()
+	print()
+	lotto_range = get_lotto()
+
 	print("Here are the euromillions numbers: ")
 	print()
-	euromillions()
+	euromillions(euro_range)
 	print()
+	
 	print("Here are the lotto numbers: ")
 	print()
-	lotto()
+	lotto(lotto_range)
+	
 	question = input("""
 	Would you like to generate numbers again?
 
