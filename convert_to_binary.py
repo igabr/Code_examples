cf3a97222143ff2166b1f37d2179c8696bf89ce2
Ibrahim Gabr <igabr@uchicago.edu>
2017-12-18 00:20:29 +0000

    Add files via upload

---
+++ convert_to_binary.py
@@ -0,0 +1,46 @@
+def convert_to_binary(val):
+    if type(val) != int:
+        raise ValueError("Please enter a valid integer")
+
+    if val == 0:
+        return "0"
+
+    binary_string = ""
+    valid_places = {}
+    invalid_places = {}
+
+    base2calc = -1
+
+    starting_exponent = 0
+
+    while base2calc <= val:
+        base2calc = 2 ** starting_exponent
+        starting_exponent += 1
+
+    highest_power = starting_exponent - 1
+
+    power_list = sorted(map(lambda x: 2**x, list(range(highest_power))),
+                        reverse=True)
+
+    for i in power_list:
+        if val - i >= 0:
+            val = val - i
+            valid_places[i] = "1"
+        else:
+            continue
+
+    for i in power_list:
+        if i not in valid_places:
+            invalid_places[i] = "0"
+
+    valid_places = list(tuple(valid_places.items()))
+    invalid_places = list(tuple(invalid_places.items()))
+
+    valid_places.extend(invalid_places)
+
+    final = sorted(valid_places, key=lambda x: x[0], reverse=True)
+
+    for i in final:
+        binary_string += i[1]
+
+    return binary_string
